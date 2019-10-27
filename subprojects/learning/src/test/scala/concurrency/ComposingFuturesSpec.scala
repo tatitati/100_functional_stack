7596adf921cb0d5f5a3525dd7bcad54e7b54afa3
Francisco Albusac <francisco.albusac@bbc.co.uk>
2019-10-27 17:18:01 +0000

    some concurrency things

---
+++ subprojects/learning/src/test/scala/concurrency/ComposingFuturesSpec.scala
@@ -0,0 +1,48 @@
+package concurrency
+
+import org.scalatest.FunSuite
+
+import scala.concurrent.{Await, Future}
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.Await
+import scala.concurrent.duration._
+
+class ComposingFuturesSpec extends FunSuite {
+
+  def fut1: Future[Int] = Future {
+    Thread.sleep(5000)
+    println("fut1 here")
+    5
+  }
+
+  def fut2(num: Int): Future[String] = Future {
+    Thread.sleep(5000)
+    println("fut2 here")
+    s"next number is " + (num+1)
+  }
+
+  test("I can map over a future") {
+    val futureResult: Future[Int] = fut1.map{ num => num * 10}
+
+    val value: Int = Await.result(futureResult, 15 seconds)
+
+    assert(50 == value)
+  }
+
+  test("Chaining futures: map create nested Futures, sometimes we have to wait of each nested, one by one"){
+    val futureText: Future[Future[String]] = fut1.map(fut2(_))
+
+    val text1: Future[String] = Await.result(futureText, 15 seconds)
+    val text2: String = Await.result(text1, 15 seconds)
+
+    assert("next number is 6" == text2)
+  }
+
+  test("Chaining futures: With flatMap we have to wait only for a future, which is not nested"){
+    val endValue: Future[String] = fut1.flatMap(fut2(_))
+
+    val text: String = Await.result(endValue, 15 seconds)
+
+    assert("next number is 6" == text)
+  }
+}
---
+++ subprojects/learning/src/test/scala/concurrency/ParallelCollectionsSpec.scala
@@ -0,0 +1,35 @@
+package concurrency
+
+import org.scalatest.FunSuite
+
+import scala.collection.parallel.immutable.ParVector
+
+class ParallelCollectionsSpec extends FunSuite {
+  test("parallel collections dont follow any order"){
+    val numbers = List(1, 2, 3, 4, 5, 6, 7, 8)
+
+    val result = numbers.par.foreach{ println }
+    println(result)
+    //    3
+    //    2
+    //    4
+    //    1
+    //    5
+    //    6
+    //    7
+    //    8
+    //    ()
+  }
+
+  test("par.map is the parallel version of map") {
+    val numbers = List(1, 2, 3, 4, 5, 6, 7, 8)
+    val result = numbers.map(_.toString)
+    assert(List("1", "2", "3", "4", "5", "6", "7", "8") == result)
+  }
+
+  test("par.map dfasdfasdf") {
+    val numbers = List(1, 2, 3, 4, 5, 6, 7, 8)
+    val result = numbers.par.map( _ * 10)
+    assert(ParVector(10, 20, 30, 40, 50, 60, 70, 80) == result)
+  }
+}
