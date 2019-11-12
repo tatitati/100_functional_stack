d2b2ba461a148b291c1f3e0de076b120fcf5c388
Francisco Albusac <francisco.albusac@bbc.co.uk>
2019-11-12 17:19:09 +0000

    more

--- subprojects/learning/src/test/scala/doobie/InsertUpdateSpec.scala
+++ subprojects/learning/src/test/scala/doobie/InsertUpdateSpec.scala
@@ -8,7 +8,7 @@ import doobie._
 
 class InsertUpdateSpec extends FunSuite with CustomDbConnection {
 
-  test("CREATE TABLE / DROP TABLE"){
+  test("Doobie: CREATE TABLE / DROP TABLE"){
     val drop: ConnectionIO[Int] = sql"""
       DROP TABLE IF EXISTS person
     """.update.run
@@ -27,28 +27,28 @@ class InsertUpdateSpec extends FunSuite with CustomDbConnection {
 
   }
 
-  test("INSERT"){
+  test("Doobie: INSERT"){
     case class Person(id: Long, name: String, age: Option[Short])
 
-    def insert1(name: String, age: Option[Short]): Update0 =
+    def insert(name: String, age: Option[Short]): Update0 =
       sql"insert into person (name, age) values ($name, $age)".update
 
-    val idRow1: Int = insert1("Alice", Some(12))
+    val idNewPerson: Int = insert("Alice", Some(12))
       .run
       .transact(xa)
       .unsafeRunSync
 
-    val personResult = sql"select id, name, age from person"
+    val listPerson = sql"select id, name, age from person"
       .query[Person]
       .to[List]
       .transact(xa)
       .unsafeRunSync
 
-    assert(1 == idRow1)
-    assert(List(Person(1,"Alice",Some(12))) == personResult)
+    assert(1 == idNewPerson)
+    assert(List(Person(1,"Alice",Some(12))) == listPerson)
   }
 
-  test("UPDATE"){
+  test("Doobie: UPDATE"){
     case class Person(id: Long, name: String, age: Option[Short])
     val resultUpdate = sql"update person set age = 15 where name = 'Alice'".update.run.transact(xa).unsafeRunSync
 
@@ -58,7 +58,7 @@ class InsertUpdateSpec extends FunSuite with CustomDbConnection {
     assert(List(Person(1,"Alice",Some(15))) == resultSelect)
   }
 
-  test("RETRIEVING ID on INSERT"){
+  test("Doobie: RETRIEVING ID on INSERT"){
     case class Person(id: Long, name: String, age: Option[Short])
     def insert2(name: String, age: Option[Short]): ConnectionIO[Person] =
       for {
