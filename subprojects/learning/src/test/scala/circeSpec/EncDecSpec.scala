2d21a475685be0ea48105e4b093973c6e0b1368e
Francisco Albusac <francisco.albusac@bbc.co.uk>
2019-11-13 23:31:26 +0000

    more

--- subprojects/learning/src/test/scala/circeSpec/EncDecSpec.scala
+++ subprojects/learning/src/test/scala/circeSpec/EncDecSpec.scala
@@ -1,7 +1,19 @@
 package circeSpec
 
+import io.circe.Json
 import org.scalatest.FunSuite
+import io.circe.syntax._
 
 class EncDecSpec extends FunSuite {
 
+  test("Encode"){
+    val intsJson: Json = List(1, 2, 3).asJson
+    //println(intsJson)
+    //  [
+    //    1,
+    //    2,
+    //    3
+    //  ]
+
+  }
 }
